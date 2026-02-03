87b1d536754e3275bf9065b484f6bd6c8f0697aa
POK <pok@Pattys-Mac-Studio.local>
2026-02-03 12:31:51 -0800

    dropdown style updates

--- custom-link-list.php
+++ custom-link-list.php
@@ -8,7 +8,7 @@
  * Primary Branch: main
  * Description: A custom post type and a block for creating and managing lists of links.
  * Author: Patty O'Hara
- * Version: 1.0.18
+ * Version: 1.0.19
  * Author URI https://carkeekstudios.com/
  * Text Domain: carkeek-blocks
  */
